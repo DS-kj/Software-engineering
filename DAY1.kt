59fd3aa274994d88be0f2da9d4bd5fd247d95610
kunjangsherpa <kunjangsherpa07@email.com>
2025-11-01 16:35:46 +0545

    assignment

---
+++ DAY1.kt
@@ -0,0 +1,52 @@
+package com.example.a37akunjang
+
+import kotlinx.serialization.descriptors.PrimitiveKind
+
+fun main() {
+//      println("YO ")
+//      print("asad")
+////        String name = "hello"
+//        var name : String ="kunjang"
+//        val age : Int = 18
+//        val terms : Boolean =false
+//        val weight : Double =0.0
+
+
+//    val name: String ="kunjang"
+//    val age : Int = 18;
+////    println("My name is $name and age is $age")        concatination
+//    println("My name is ${name.uppercase()} and age is $age")        interpolation
+//
+//
+//    val age =arrayOf(10,20,30)
+//    age[2]=50
+//    println(age[2])
+
+
+//    val age1 =ArrayList<Int>()
+//    age1.add(10)
+//    val age = arrayListOf<Int>(10,20,30)
+
+
+//    for(i in 0 .. 10){
+//        print(i)
+//    }
+//
+//    for(i in 0 until 10){
+//        print(i)
+//    }
+//    for(i in 0 until 10 step 2){
+//        print(i)
+//    }
+//    for(i in 10 downTo  0 ){
+//        print(i)
+//    }
+
+
+    fun calculation(a: Int, b: Int): Int {
+        return a + b
+    }
+
+
+
+}
